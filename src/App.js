a3e65fa79988167679e044ab6ecb8be6460c5590
Lokesh Yadav <i_nydv@hotmail.com>
2022-04-04 19:17:07 +0530

    Adde login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TopBar from "./component/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/singlepage/Single";
 import Write from "./pages/write/Write";
@@ -11,7 +12,8 @@ function App() {
       {/* <Home /> */}
       {/* <Single /> */}
       {/* <Write /> */}
-      <Settings />
+      {/* <Settings /> */}
+      <Login />
     </div>
   );
 }
