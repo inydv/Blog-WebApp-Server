0725a9cfaebf40333fa6aa2dd5afc3ac411b35c9
Lokesh Yadav <i_nydv@hotmail.com>
2022-04-22 18:52:25 +0530

    small change in posts.js for empty post

--- src/component/posts/Posts.js
+++ src/component/posts/Posts.js
@@ -4,9 +4,13 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts.map((item) => (
-        <Post post={item} key={item._id} />
-      ))}
+      {posts.length ? (
+        posts.map((item) => <Post post={item} key={item._id} />)
+      ) : (
+        <h3 style={{ margin: "0px 20px", color: "red" }}>
+          Nothing Pusblished Yet...
+        </h3>
+      )}
     </div>
   );
 }
